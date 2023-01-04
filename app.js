78c06d0d9fa09eb6fe6eb32197e93bac3940be8e
Ryan <mills.ryan.011@gmail.com>
2023-01-04 08:11:55 -0500

    forEach inline and named functions

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+const numbers = [1,2,3,4,5,6,,7,8,9,0,10,11,12,13];
+
+function print(element) {
+    console.log(element);
+}
+
+numbers.forEach(print);
+
+// we can also declare functions in-line, which is much more common.
+
+numbers.forEach(function(element) {
+    console.log(element);
+})
